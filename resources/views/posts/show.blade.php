6c821eefc0f2c6a4cfaafb0fd3ce8e2249c10ebd
Farhad Hossain <16303038@iubat.edu>
2020-06-10 18:23:45 +0600

    Full Complete

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+
+<a href="/posts" class="btn btn-default"> Go Back </a>
+
+<hr>
+
+<h1>{{$post->title}}</h1>
+<div  class="row">
+	
+	<div class="col-md-12">
+		<img src="/storage/cover_image/{{$post->cover_image}}" alt="">
+	</div>
+</div>
+
+
+<p> {{$post->body}} </p>
+<hr>
+
+
+<small>Written on {{$post->created_at}} </small>
+
+<hr>
+
+@if(!Auth::guest())
+
+	@if(Auth::user()->id == $post->user_id)
+		<a href="/posts/{{$post->id}}/edit" class="btn btn-default"> Edit</a>
+		{!!Form::open(['action'=>['PostsController@destroy',$post->id],'method' => 'POST', 'class' => 'pull-right'])!!}
+		{{Form::hidden('_method','DELETE')}}
+		{{Form::submit('Delete', ['class'=> 'btn btn-danger'])}}
+		{!!Form::close()!!}
+	@endif
+@endif
+@endsection
